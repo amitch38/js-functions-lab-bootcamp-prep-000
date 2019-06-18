837fc3d9df056c2aee0fb743776aa48bc84ab32a
Aaron M <amitchtech@gmail.com>
2019-06-18 20:04:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ function holidayCountdown(days, holiday) {
 
 happyHolidaysTo("you")
 happyHolidayTo("Independence Day", "you")
-holidayCountdown("20", "Mother's Day")
+holidayCountdown("Mother's Day", "20")
